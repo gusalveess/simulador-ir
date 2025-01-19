0a8e7cb200ba06704b4b326ecac9b59f7b0d76a2
gusalveess <gusalveess@Gustavo.>
2025-01-18 22:46:54 -0300

    [FEATURE] Adicionada a documentação da API com swagger, configuração do GitHub CI/CD e criação do README
    - Adicionada documentação Swagger para a API
    - Configurado pipeline de CI/CD com GitHub Actions (Lint, Teste, Build)
    - Criado README com instruções de uso e configuração do projeto

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import * as swaggerUi from 'swagger-ui-express';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as YAML from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT ?? 3000);
+
+  const swaggerPath = path.resolve(__dirname, '../src', 'swagger.yaml');
+
+  if (!fs.existsSync(swaggerPath)) {
+    console.error(`Arquivo Swagger não encontrado em: ${swaggerPath}`);
+    process.exit(1);
+  }
+
+  try {
+    const swaggerDocument = YAML.load(swaggerPath);
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  } catch (error) {
+    console.error('Erro ao ler ou processar o arquivo Swagger:', error);
+    process.exit(1);
+  }
+
+  await app.listen(process.env.PORT ?? 8000);
 }
+
 bootstrap();
